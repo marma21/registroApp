79c642fafde32b4338ef4c8b28009c0df213baac
Mauro <martini.mauro@gmail.com>
2020-03-29 14:23:54 -0300

    git remote add origin https://github.com/marma21/registroApp.git

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router'
-import { DatabaseService, Reg ,GroupedByfecha,Regbyfecha} from '../services/database.service';
+import { DatabaseService, Reg, GroupedByfecha, Regbyfecha } from '../services/database.service';
 import { GroupByPipe } from 'ngx-pipes';
-
+import { File } from '@ionic-native/file/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,20 +12,21 @@ import { GroupByPipe } from 'ngx-pipes';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  groupedByfecha:GroupedByfecha;
-  dataReturned:Array<Reg>;
+  groupedByfecha: GroupedByfecha;
+  dataReturned: Array<Reg>;
   registros: Reg[] = [];
-  regbyfecha: Regbyfecha[]=[];
+  regbyfecha: Regbyfecha[] = [];
+  csvcontent:any;
 
-  constructor(private db: DatabaseService, private router:Router, private groupby:GroupByPipe) {}
+  constructor(private db: DatabaseService, private router: Router, private groupby: GroupByPipe, private file: File, private plt: Platform, private socialSharing: SocialSharing) { }
 
   ngOnInit() {
     this.db.getDatabaseState().subscribe(rdy => {
       if (rdy) {
         this.db.getRegs().subscribe(regs => {
-          this.groupedByfecha = this.groupby.transform(regs, ['fecha','lote'], ' Lote:' );
+          this.groupedByfecha = this.groupby.transform(regs, ['fecha', 'lote'], ' Lote:');
           this.regbyfecha = [];
-          for (const property in this.groupby.transform(regs, ['fecha','lote'], ' Lote:' )) {
+          for (const property in this.groupby.transform(regs, ['fecha', 'lote'], ' Lote:')) {
             this.regbyfecha.push({
               fecha: property,
               enviar: false,
@@ -36,11 +39,37 @@ export class Tab1Page {
   }
 
   addRegistro() {
-    
-    this.db.addRegistro()
-    .then(data => {
-      this.router.navigateByUrl('/tabs/tab2/'+data);
-    });
-  }
 
+    this.db.addRegistro()
+      .then(data => {
+        this.router.navigateByUrl('/tabs/tab2/' + data);
+      });
+  };
+
+  sendRegistros() {
+    let csv: any = '';
+    for (const property in this.regbyfecha) {
+      if (this.regbyfecha[property].enviar) {
+        csv += this.convertToCSV(this.regbyfecha[property]);
+      };
+    };
+    if (this.plt.is("cordova")) {
+      this.file.writeFile(this.file.dataDirectory, 'data.csv', csv, { replace: true }).then(res => {
+
+        this.socialSharing.share('Registros de carga' + '\r\n' + csv, null, res.nativeURL, null);
+      });
+    }
+  };
+
+  convertToCSV(registro) {
+    let line: any = ''
+    line += 'Fecha y Lote' + '\r\n';
+    line += registro.fecha + '\r\n';
+    line += 'Fecha;Lote;Tipo;Camion;Patente/Bolson;Total' + '\r\n';
+    registro.registros.forEach(element => {
+      line += element.fecha + ',' + element.lote + ','+ element.tipo + ','+ element.camion + ',' + element.patente + ',' + element.total + '\r\n';
+    });
+
+    return line;
+  }
 }
